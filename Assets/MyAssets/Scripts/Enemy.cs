a26f5f7706327756d6c68ca3d7505639a914759b
Oscar Mora <oscare.ramirez@alumnos.udg.mx>
2021-12-09 11:12:36 -0600

    Add AI navigation

--- Assets/MyAssets/Scripts/Enemy.cs
+++ Assets/MyAssets/Scripts/Enemy.cs
@@ -1,12 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour, IArrowHittable
 {
+    [SerializeField]
+    private int health = 100;
+
+    private Transform target;
+    private NavMeshAgent agent;
+
     public void Hit(Arrow arrow)
     {
         Debug.Log(name + " was hit");
+        health -= 100;
+
+        if (health <= 0)
+        {
+            Death();
+        }
+    }
+
+    private void Death()
+    {
+        // Play Death animation
+        // Play sound
+        agent.enabled = false;
+        GetComponent<Animator>().enabled = false;
+        Destroy(this, 15);
     }
 
     /*private void ApplyMaterial()
@@ -22,12 +44,13 @@ public class Enemy : MonoBehaviour, IArrowHittable
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Target").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.SetDestination(target.position);
     }
 }
